f50b5335d03e784dfc4daac4432ef2f25267ec2e
areverdy <alec.rev1234@gmail.com>
2023-09-13 10:59:52 +0200

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world');
+import express from "express"
+import "dotenv/config"
+
+const app = express()
+const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
+
+app.get('/hello', (req, res) => {
+  res. send('Hello World!')
+})
+
+app. listen(port, () => {
+  console.log(`Listening port on ${port}`)
+})
