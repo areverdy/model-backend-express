17e0a8949868a7128352fc32829fd0341aecdace
areverdy <alec.rev1234@gmail.com>
2023-09-14 16:44:04 +0200

    ajout cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import "dotenv/config"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
+app.use(cors())
 
 app.get('/hello', (req, res) => {
   res. send('Hello World!')
