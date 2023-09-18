66214772af64fb8feb1cfe2ada3ff4b3459ad610
areverdy <alec.rev1234@gmail.com>
2023-09-18 13:21:11 +0200

    add cors and body parser deps

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import bodyParser from "body-parser"
 
 const app = express()
 const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/hello', (req, res) => {
   res. send('Hello World!')
